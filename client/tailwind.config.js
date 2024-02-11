f30da061eac6cc71a4cb8b1ae59cd84be03aec08
Gabyp05 <gabyapd05@gmail.com>
2024-02-10 21:10:10 -0400

    Implement new colors and make changes to main page

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,26 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#2E2E3E",
+        secondary: "#E0E1EB",
+        paragraph: "#E0E0E6",
+      },
+      fontFamily: {
+        monse: ["Montserrat", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
   },
   plugins: [],
-}
+};
