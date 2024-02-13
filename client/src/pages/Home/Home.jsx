50f82f1f96838f791fe1269a9424d4fc696d88da
Gabyp05 <gabyapd05@gmail.com>
2024-02-13 17:07:58 -0400

    Hero section

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import styles from '../../style';
+import Hero from '../../components/Hero';
+
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className={`bg-primary ${styles.flexStart} flex-col`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+    </div>
   )
 }
 
