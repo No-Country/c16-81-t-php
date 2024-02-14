bf7565a34798d5e2960a6e8326d2f20478323578
Nehuen Cuenca <nehuen.cuenca@hotmail.com>
2024-02-13 21:52:57 -0300

    created the CRUD routes for Team model, implementing resource controller, adding form request for controller's storage & update methods

---
+++ server/app/Http/Controllers/TeamController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Team;
+use App\Http\Requests\StoreTeamRequest;
+use App\Http\Requests\UpdateTeamRequest;
+use Illuminate\Support\Facades\DB;
+
+class TeamController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $team =  Team::all();
+        return $team;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTeamRequest $request)
+    {
+        Team::create($request->all());
+        return response()->json([
+            "success" => true,
+            "msg" => "Team created successfully"
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Team $team)
+    {
+        return response()->json([
+            "success" => true,
+            "Team" => $team
+        ], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTeamRequest $request, Team $team)
+    {
+        $team->update($request->all());
+        return response()->json([
+            "success" => true,
+            "msg" => "Team update successfully"
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Team $team)
+    {
+        $team->delete();
+        return response()->json([
+            "success" => true,
+            "msg" => "Team deleted successfully"
+        ], 200);
+    }
+
+    // public function showIntegrants($id)
+    // {
+    //     $integrants = DB::table('')
+    //     return response()->json([
+    //         "success" => true,
+    //         "msg" => "Team deleted successfully"
+    //     ], 200);
+    // }
+}
--- server/routes/api.php
+++ server/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ConfrontationController;
+use App\Http\Controllers\TeamController;
 use App\Http\Controllers\TournamentController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
@@ -29,3 +30,6 @@ Route::resource('confrontations', ConfrontationController::class);
 
 /**Route By Tournaments */
 Route::get('/tournaments/{id}/confrontations', [TournamentController::class, 'showConfrontations']);
+
+/**Route By Teams */
+Route::resource('teams', TeamController::class);
