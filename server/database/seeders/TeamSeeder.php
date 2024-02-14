9f2c7f3997d72410863d0445a97b3ee5370f246f
Nehuen Cuenca <nehuen.cuenca@hotmail.com>
2024-02-13 21:48:13 -0300

    created TeamSeeder to seed 20 fake teams for each user

---
+++ server/database/seeders/TeamSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+class TeamSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+
+
+        for ($i = 0; $i < 20; $i++) {
+            $nameUserLeader = User::where('id', $i+1)->first()->nick_name;
+            DB::table('teams')->insert([
+                'name' => $nameUserLeader."'s team",
+                'user_leader_id'  => $i+1,
+            ]);
+        }
+    }
+}
