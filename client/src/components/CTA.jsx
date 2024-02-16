f1c3773812ddba20f1d9955d8086b944e90298af
Gabriela Patiño <gabyapd05@gmail.com>
2024-02-16 15:38:00 -0400

    Fix cta

--- client/src/components/CTA.jsx
+++ client/src/components/CTA.jsx
@@ -13,13 +13,13 @@ const CTA = () => {
                     <p className="font-monse font-normal text-[14px] sm:text-[16px] text-[#5C5D75] text-pretty text-center sm:text-start px-[50px] sm:px-0 leading-7">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the</p>
                 </div>
                 <div className="flex items-center justify-start shadow-xl">
-                    <input type="email" className="w-auto h-[64px] bg-[#3E3E54] text-secondary outline-none rounded-[14px] py-1 px-6 " placeholder="Ingresa tu email..."/>
+                    <input type="email" className="w-auto h-[50px] sm:h-[64px] bg-[#3E3E54] text-secondary outline-none rounded-[14px] py-1 px-6 " placeholder="Ingresa tu email..."/>
                     <button type='button'           
                             className={`hidden w-[174px] h-[64px] py-1 px-6  bg-gray-gradient flex-1 sm:flex gap-2 flex-row items-center justify-between font-monse font-normal text-[16px] text-white hover:text-secondary outline-none rounded-[14px] ml-[-20px] sm:ml-[-40px]`}> 
                         Suscribir <img src={icon} alt='Icon' />
                     </button>
                     <button type='button'           
-                            className={`sm:hidden w-[auto h-[64px] py-1 px-4 bg-gray-gradient gap-2 flex flex-row items-center justify-between font-monse font-normal text-[16px] text-white hover:text-secondary outline-none rounded-[14px] ml-[-60px]`}> 
+                            className={`sm:hidden w-[auto] h-[50px] sm:h-[64px] py-1 px-4 bg-gray-gradient gap-2 flex flex-row items-center justify-between font-monse font-normal text-[16px] text-white hover:text-secondary outline-none rounded-[14px] ml-[-60px]`}> 
                         Suscribir <img src={icon} alt='Icon' />
                     </button>
                 </div>  
