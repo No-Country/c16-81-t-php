fa83f426139d74254c7ebc88f3fabde38a825a49
Nehuen Cuenca <nehuen.cuenca@hotmail.com>
2024-02-16 16:24:07 -0300

    created model, migration, seeder and factory for position table. Applied seeder

--- server/database/seeders/DatabaseSeeder.php
+++ server/database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             TeamSeeder::class,
             TeamUserSeeder::class,
             StatusSeeder::class,
+            PositionSeeder::class,
             ConfrontationSeeder::class,
         ]);
     }
