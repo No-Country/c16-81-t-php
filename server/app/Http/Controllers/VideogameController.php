2504ad65705f937185bc2f1b59714e44d432ee38
Nehuen Cuenca <nehuen.cuenca@hotmail.com>
2024-02-17 13:51:26 -0300

    Recreate the VideogameController to mantain the Resource Controller structure, add the form requests with rules

---
+++ server/app/Http/Controllers/VideogameController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Videogame;
+use App\Http\Requests\StoreVideogameRequest;
+use App\Http\Requests\UpdateVideogameRequest;
+
+class VideogameController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $videogames = Videogame::all();
+        return response()->json([
+            "success" => true,
+            "videogames" => $videogames
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreVideogameRequest $request)
+    {
+        $videogame = Videogame::create($request->all());
+        return response()->json([
+            "success" => true,
+            "msg" => "Videogame created successfully",
+            "videogame" => $videogame
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Videogame $videogame)
+    {
+        return response()->json([
+            "success" => true,
+            "videogame" => $videogame
+        ], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateVideogameRequest $request, Videogame $videogame)
+    {
+        $videogame->update($request->all());
+        return response()->json([
+            "success" => true,
+            "msg" => "Videogame upated successfully",
+            "videogame" => $videogame
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Videogame $videogame)
+    {
+        $videogame->delete();
+        return response()->json([
+            "success" => true,
+            "msg" => "Videogame deleted successfully"
+        ], 200);
+    }
+}
