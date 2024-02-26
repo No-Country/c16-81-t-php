efc2a4f2a234a5773ae7605c54e187a1aab7ac85
Nehuen Cuenca <nehuen.cuenca@hotmail.com>
2024-02-26 15:53:56 -0300

    Implement react-hook-form with validations to each fields

--- client/src/pages/Dashboard/Profile/ChangePassword.jsx
+++ client/src/pages/Dashboard/Profile/ChangePassword.jsx
@@ -1,27 +1,89 @@
 import styles from "../../../style"
 
+import { useForm } from 'react-hook-form';
+
 const ChangePassword = () => {
   const { labelElement, inputElement } = styles
 
+  const { register, reset, handleSubmit, formState: { errors } } = useForm(
+    {
+      defaultValues: {
+        old_password: "", //"testingPassword123",
+        new_password: "", //"newTestingPasswordABC",
+        confirm_password: "", //"newTestingPasswordABC",
+      },
+    }
+  );
+  const onSubmit = (data) => {
+    console.log(data);
+    const confirmPasswordMatch = data.new_password === data.confirm_password
+    if(!confirmPasswordMatch){
+      alert('La confirmacion de contraseña no coincide con la contraseña nueva especificada')
+    }
+  }
+
+
   return (
-    <form id="changePasswordForm" className={`min-h-[58vh] flex flex-col justify-between gap-y-20 p-6 border-2 border-black rounded-lg `}>
+    <form id="changePasswordForm" onSubmit={handleSubmit(onSubmit)} className={`min-h-[58vh] flex flex-col justify-between gap-y-20 p-6 border-2 border-black rounded-lg `}>
         <div className="flex flex-wrap gap-4">
           <div className="flex flex-col">
-            <label htmlFor="oldPassword" className={`${labelElement}`}>Contraseña anterior</label>
-            <input type="password" name="oldPassword" id="oldPassword" className={`${inputElement}`} />
+            <label htmlFor="old_password" className={`${labelElement}`}>Contraseña anterior</label>
+            <input type="password" name="old_password" id="old_password" className={`${inputElement}`}
+            {...register("old_password", {
+              required: {
+                  value: true,
+                  message: "La contraseña anterior es requerida",
+                }
+              })
+            }
+            />
+            {errors.old_password?.type === "required" && <span className='text-red-500'>La contraseña anterior es requerida</span>}
+
           </div> 
           <div className="flex flex-col">
-            <label htmlFor="newPassword" className={`${labelElement}`}>Contraseña nueva</label>
-            <input type="password" name="newPassword" id="newPassword" className={`${inputElement}`} />
+            <label htmlFor="new_password" className={`${labelElement}`}>Contraseña nueva</label>
+            <input type="password" name="new_password" id="new_password" className={`${inputElement}`}
+              {...register("new_password", {
+                required: {
+                    value: true,
+                    message: "La contraseña nueva es requerida",
+                  }, 
+                  maxLength: 20, minLength: 4
+                })
+              }
+            />
+            {errors.new_password?.type === "required" && <span className='text-red-500'>La contraseña nueva es requerida</span>}
+            {errors.new_password?.type === "maxLength" && (
+            <span className='text-red-500'>La contraseña nueva no debe ser mayor a 20 caracteres</span>
+            )}
+            {errors.new_password?.type === "minLength" && (
+            <span className='text-red-500'>La contraseña nueva debe ser mayor a 4 caracteres</span>
+            )}
           </div> 
           <div className="flex flex-col">
-            <label htmlFor="confirmPassword" className={`${labelElement}`}>Confirme la contraseña</label>
-            <input type="password" name="confirmPassword" id="confirmPassword" className={`${inputElement}`} />
+            <label htmlFor="confirm_password" className={`${labelElement}`}>Confirme la contraseña</label>
+            <input type="password" name="confirm_password" id="confirm_password" className={`${inputElement}`}
+              {...register("confirm_password", {
+                required: {
+                    value: true,
+                    message: "La confirmacion es requerida",
+                  }, 
+                  maxLength: 20, minLength: 4
+                })
+              }
+            />
+            {errors.confirm_password?.type === "required" && <span className='text-red-500'>La confirmacion es requerida</span>}
+            {errors.confirm_password?.type === "maxLength" && (
+            <span className='text-red-500'>La confirmacion no debe ser mayor a 20 caracteres</span>
+            )}
+            {errors.confirm_password?.type === "minLength" && (
+            <span className='text-red-500'>La confirmacion debe ser mayor a 4 caracteres</span>
+            )}
           </div> 
         </div>
         <div className="flex justify-between">
           <button type="submit" className=" bg-activePurple px-12 py-2 rounded-lg text-xl font-bold text-card">Guardar</button>
-          <button type="reset" className=" bg-ligthPurple px-12 py-2 rounded-lg text-xl font-bold text-card">No, cancelar</button>
+          <button type="reset" onClick={reset} className=" bg-ligthPurple px-12 py-2 rounded-lg text-xl font-bold text-card">Limpiar</button>
         </div>
       </form>
   )
--- client/src/pages/Dashboard/Profile/EditProfile.jsx
+++ client/src/pages/Dashboard/Profile/EditProfile.jsx
@@ -1,9 +1,27 @@
 import styles from "../../../style";
 import { useRef } from 'react';
+import { useForm } from 'react-hook-form';
 
 const EditProfile = () => {
   const { labelElement, inputElement } = styles
   const imgPreviewElement = useRef(null);
+
+  const { register, reset, handleSubmit, formState: { errors } } = useForm(
+    {
+      defaultValues: {
+        nick_name: "", //"CharlyCharly",
+        first_name: "", //"Charly",
+        last_name: "", //"Garcia",
+        gender: "", //"male",
+        phone: "", //"1234567",
+        email: "", //"charly_garcia@fakegmail.com",
+        avatar: ""
+      },
+    }
+  );
+  const onSubmit = (data) => {
+    console.log({data});
+  }
 
   const handleChangeAvatar = (e) => { 
     const [avatar] = e.target.files
@@ -18,49 +36,116 @@ const EditProfile = () => {
   }
 
   return (
-      <form id="editProfileForm" className={`min-h-[58vh] flex flex-col gap-y-12 p-6 border-2 border-black rounded-lg`}>
+      <form id="editProfileForm" onSubmit={handleSubmit(onSubmit)} className={`min-h-[58vh] flex flex-col gap-y-12 p-6 border-2 border-black rounded-lg`}>
         <div className="flex flex-wrap gap-4">
           <div className="flex flex-col">
             <label htmlFor="nick_name" className={`${labelElement}`}>Nickname</label>
-            <input type="text" name="nick_name" id="nick_name" className={`${inputElement}`} />
+            <input type="text" name="nick_name" id="nick_name" className={`${inputElement}`}
+              {...register("nick_name", { 
+                  required: {
+                    value: true,
+                    message: "El nickname es requerido",
+                  }, 
+                  maxLength: 12, minLength: 4
+                })
+              }  />
+            {errors.nick_name?.type === "required" && <span className='text-red-500'>Nickname es requerido</span>}
+            {errors.nick_name?.type === "maxLength" && (
+            <span className='text-red-500'>Nickname no debe ser mayor a 12 caracteres</span>
+            )}
+            {errors.nick_name?.type === "minLength" && (
+            <span className='text-red-500'>Nickname debe ser mayor a 2 caracteres</span>
+            )}
           </div>
           <div className="flex flex-col">
             <label htmlFor="first_name" className={`${labelElement}`}>Primer nombre</label>
-            <input type="text" name="first_name" id="first_name" className={`${inputElement}`}/>
+            <input type="text" name="first_name" id="first_name" className={`${inputElement}`} 
+            {...register("first_name", {
+                required: {
+                  value: true,
+                  message: "El primer nombre es requerido",
+                }, 
+                maxLength: 20, minLength: 4
+              })
+            } />
+            {errors.first_name?.type === "required" && <span className='text-red-500'>El primer nombre es requerido</span>}
+            {errors.first_name?.type === "maxLength" && (
+            <span className='text-red-500'>El primer nombre no debe ser mayor a 20 caracteres</span>
+            )}
+            {errors.first_name?.type === "minLength" && (
+            <span className='text-red-500'>El primer nombre debe ser mayor a 4 caracteres</span>
+            )}
           </div>
           <div className="flex flex-col">
             <label htmlFor="last_name" className={`${labelElement}`}>Apellido</label>
-            <input type="text" name="last_name" id="last_name" className={`${inputElement}`}/>
+            <input type="text" name="last_name" id="last_name" className={`${inputElement}`}
+            {...register("last_name", {
+                required: {
+                  value: true,
+                  message: "El apellido es requerido",
+                },
+                maxLength: 20, minLength: 4
+              })
+            } />
+            {errors.last_name?.type === "required" && <span className='text-red-500'>El apellido es requerido</span>}
+            {errors.last_name?.type === "maxLength" && (
+            <span className='text-red-500'>El apellido no debe ser mayor a 20 caracteres</span>
+            )}
+            {errors.last_name?.type === "minLength" && (
+            <span className='text-red-500'>El apellido debe ser mayor a 4 caracteres</span>
+            )}
           </div>
           <div className="flex flex-col">
             <label htmlFor="email" className={`${labelElement}`}>Email</label>
-            <input type="email" name="email" id="email" className={`${inputElement}`}/>
+            <input type="email" name="email" id="email" className={`${inputElement}`}
+            {...register("email", { 
+                required: {
+                  value: true,
+                  message: "El email es requerido",
+                }
+              })
+            } />
+            {errors.email?.type === "required" && <span className='text-red-500'>El email es requerido</span>}
           </div>
           <div className="flex flex-col">
             <label htmlFor="phone" className={`${labelElement}`}>Telefono</label>
-            <input type="tel" name="phone" id="phone" className={`${inputElement}`}/>
+            <input type="tel" name="phone" id="phone" className={`${inputElement}`}
+            {...register("phone", {
+                required: {
+                  value: true,
+                  message: "El telefono es requerido",
+                }
+              })
+            } />
+            {errors.phone?.type === "required" && <span className='text-red-500'>El telefono es requerido</span>}
           </div>
           <div className="flex flex-col">
             <label htmlFor="gender" id="gender" className={`${labelElement}`}>Genero</label>
-            <select
-              name="gender"
-              className={`${inputElement}`}
+            <select name="gender" className={`${inputElement}`}
+              {...register("gender", {
+                  required: {
+                    value: true,
+                    message: "El genero es requerido",
+                  } 
+                })
+              }
             >
               <option value="male">Masculino</option>
               <option value="female">Femenino</option>
               <option value="other">Otro</option>
             </select>
+            {errors.gender?.type === "required" && <span className='text-red-500'>El genero es requerido</span>}
           </div>
 
           <div className="flex flex-col gap-y-2">
             <label htmlFor="avatar" className={`${labelElement}`}>Avatar</label>
-            <input type="file" id="avatar" name="avatar" accept="image/png, image/jpeg" onChange={handleChangeAvatar} />
+            <input type="file" {...register("avatar")} id="avatar" name="avatar" accept="image/png, image/jpeg" onChange={handleChangeAvatar} />
             <img id="blah" src="#" onError={handleEmptyAvatar} alt="Preview avatar" ref={imgPreviewElement} className="rounded-full text-center text-transparent" width={150} height={150}/>
           </div>
         </div>
         <div className="flex justify-between">
           <button type="submit" className="bg-activePurple px-12 py-2 rounded-lg text-xl font-bold text-card">Guardar</button>
-          <button type="reset" className="bg-ligthPurple px-12 py-2 rounded-lg text-xl font-bold text-card">Limpiar</button>
+          <button type="reset" onClick={reset} className="bg-ligthPurple px-12 py-2 rounded-lg text-xl font-bold text-card">Limpiar</button>
         </div>
       </form>
   );
