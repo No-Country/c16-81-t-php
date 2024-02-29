81cef39a14f36abf686dbd20a701d29d7f38eb79
Nehuen Cuenca <nehuen.cuenca@hotmail.com>
2024-02-29 17:24:02 -0300

    Add a component that displays the teams where the user is participating, with pagination

--- client/src/pages/Dashboard/Team/Integrating.jsx
+++ client/src/pages/Dashboard/Team/Integrating.jsx
@@ -4,33 +4,53 @@ import Pagination from '../../../components/Dashboard/Pagination'
 
 const Integrating = () => {
   const [teams, setTeams ] = useState([])
+  const [currentPage, setCurrentPage] = useState(1)
+  const [totalPages, setTotalPages] = useState(0)
+
+  const paginatedBy = 8
 
   useEffect(() => {
     const getTeams = async() => { 
       try {
-        const resp = await fetch("http://127.0.0.1:8000/api/teams")
+        const token = localStorage.getItem(import.meta.env.VITE_USER_TOKEN_NAME)
 
+        const resp = await fetch(`http://127.0.0.1:8000/api/users/participating-teams?page=${currentPage}&paginatedBy=${paginatedBy}`,{
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+            Accept: "application/json",
+            withCredentials: "true",
+          },
+        })
+
+        const data = await resp.json()
         if(!resp.ok){
-          throw new Error('Error while trying to fetch the teams from the API')
+          throw new Error(`Ocurrio un error a la hora de conseguir los equipos participes por el usuario: ${data.message}`)
         }
 
-        const teams = await resp.json()
-        return teams
+        setTotalPages(data.participating_in.last_page)
+
+        return data.participating_in.data
       } catch (error) {
         console.error(error)
-        alert(error)
         return []
       }
     }
 
     getTeams()
-        .then( setTeams )
-  }, [])
+        .then(setTeams)
+        .catch(setTeams)
+  }, [currentPage, totalPages])
 
   return (
     <div className={`min-h-[58vh] w-full flex flex-col justify-between gap-y-30 p-6 border-2 border-black rounded-lg`}>
-      { teams.length > 0 && <TeamsList teams={teams} className="w-full flex flex-wrap gap-y-6 gap-x-12"/>}
-      { teams.length > 0 && <Pagination className={"flex gap-x-2"} />}
+      { teams.length > 0 &&
+        <>
+          <TeamsList teams={teams} className="w-full flex flex-wrap gap-y-6 gap-x-12"/>
+          <Pagination currentPage={currentPage} setPage={setCurrentPage} totalPages={totalPages}  className={"flex gap-x-2"} />
+        </>
+      }
     </div>
   )
 }
