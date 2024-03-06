e6f0502e97b8c5ea8c745584951cfeac0ab74ddc
Nehuen Cuenca <nehuen.cuenca@hotmail.com>
2024-03-05 23:00:53 -0300

    Removed index confrontation route. Add store, update and destroy methods with validations to ConfrontationControllers

--- server/app/Http/Controllers/ConfrontationController.php
+++ server/app/Http/Controllers/ConfrontationController.php
@@ -12,13 +12,6 @@ use Illuminate\Http\Request;
 
 class ConfrontationController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
