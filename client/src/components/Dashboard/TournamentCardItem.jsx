777ed7f87f695dc33ded0f5b62abfed5b56d6302
Gabriela Patiño <gabyapd05@gmail.com>
2024-03-07 15:09:17 -0400

    update TournamentCardItem link to tournament page

--- client/src/components/Dashboard/TournamentCardItem.jsx
+++ client/src/components/Dashboard/TournamentCardItem.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom"
 
-const TournamentCardItem = ({ tournament }) => {
+const TournamentCardItem = ({ tournament }) => { 
     const parsedName = (name) => { 
         return (name.length > 15)
         ? name.slice(0, 10).concat('...')
@@ -9,7 +9,7 @@ const TournamentCardItem = ({ tournament }) => {
   return (
     <li className="flex flex-col gap-2 bg-ligthPurple px-3 pt-4 pb-2 rounded-lg h-auto">
       <img src={tournament.image} alt={tournament.title} width={150} height={100} className="rounded-md" />
-      <Link to="#" className="font-semibold block">{parsedName(tournament.name)}</Link>
+      <Link to={`/tournament/${tournament.id}`} className="font-semibold block">{parsedName(tournament.name)}</Link>
       <span className="font-light text-[12px]">{tournament.modality}</span>
     </li>
 
