6b2cd432f5b510ab31ffdd50575c49018c4bdef5
Nehuen Cuenca <nehuen.cuenca@hotmail.com>
2024-03-07 09:36:38 -0300

    Fix re-render caused by useEffects. Removed required values on form

--- client/src/pages/Tournaments/Tournament/InfoTournament.jsx
+++ client/src/pages/Tournaments/Tournament/InfoTournament.jsx
@@ -1,74 +1,41 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { coupon, calendar, time} from "../../../assets";
 import { useParams, useNavigate } from 'react-router-dom';
 import Results from './Results';
-import { verifyAuthorization } from "../../../helpers/callsToAPI"
+import { getTournamentById } from "../../../helpers/callsToAPI"
 import { useForm } from 'react-hook-form';
 import styles from '../../../style';
+import { cleanFormDataValues } from '../../../helpers/forms';
 
 const Tournaments = () => {
   const { labelElement, inputElement } = styles
 
   const navigate = useNavigate();
-  useEffect(() => {
-    // Dirigirse a la parte superior de la página al cargar la página del torneo
-    window.scrollTo(0, 0);
-  }, []);
-
-  const getCurrentUserId = async () => {
-    await verifyAuthorization(); 
-    const currentUserId = localStorage.getItem('currentUserId'); // Obtener el currentUserId del localStorage
-    return currentUserId;
-  };
 
   const [isAdmin, setIsAdmin] = useState(false);
-  const [torneoInfo, setTorneoInfo] = useState(null);
+  const [tournamentInfo, setTournamentInfo] = useState(null);
   const { tournamentId } = useParams();
 
+  const fetchTournamentInfo = useCallback(async () => {
+    const tournament = await getTournamentById(tournamentId);
+    setTournamentInfo(tournament);
+  }, [])
+
   useEffect(() => {
-    const getTorneoInfo = async () => {
-      try {
-        const token = localStorage.getItem(
-          import.meta.env.VITE_USER_TOKEN_NAME
-        );
+    fetchTournamentInfo()
+      .catch(console.error);
+       
+  }, [fetchTournamentInfo]);
   
-        const resp = await fetch(`http://127.0.0.1:8000/api/tournaments/${tournamentId}`, {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${token}`,
-            Accept: "application/json",
-            withCredentials: "true",
-          },
-        });
-  
-        const data = await resp.json();
-        if (!resp.ok) {
-          throw new Error(data.message);
-        }
-  
-        setTorneoInfo(data); 
-      } catch (error) {
-        console.error(
-          `Error al traer datos del torneo: ${error.message}`
-        );
-        setTorneoInfo(null);
-      }
-    };
-  
-    const checkAdminStatus = async () => {
-      await getTorneoInfo(); // Obtener la información del torneo al cargar el componente
-      const currentUserId = await getCurrentUserId(); // Obtener el ID del usuario actual
-      const managedById = torneoInfo?.tournament?.managed_by.id; // Obtener el ID del administrador del torneo
-
-      if (currentUserId && managedById && currentUserId.toString() === managedById.toString()) {
-        setIsAdmin(true);
-      } else {
-        setIsAdmin(false);
-      }
-    };
-
-    checkAdminStatus(); // Verificar el estado de administrador al cargar el componente
-  }, [tournamentId, torneoInfo]);
+  useEffect(() => {
+    const currentUserId = Number(localStorage.getItem('currentUserId'))
+    const managedById = tournamentInfo?.managed_by.id;
+    const areTruthy = !!currentUserId && !!managedById
+    if(areTruthy && (currentUserId === managedById)){
+      setIsAdmin(true)
+    }
+ 
+  }, [tournamentInfo])
   
 
   function formatDate(date) {
@@ -90,7 +57,7 @@ const Tournaments = () => {
     };
   }
 
-  const { register, reset, handleSubmit, formState: { errors } } = useForm(
+  const { register, reset, handleSubmit, formState: {errors} } = useForm(
     {
       defaultValues: {
         quantity_teams: "",
@@ -102,18 +69,22 @@ const Tournaments = () => {
   );
 
   const onSubmit = async(formDataValues) => {
+    
+    const cleanedValues = cleanFormDataValues(formDataValues)
+
     try {
       const token = localStorage.getItem(import.meta.env.VITE_USER_TOKEN_NAME)
+      const URL_ENVIROMENT = import.meta.env.VITE_URL_LOCAL
 
-      const resp = await fetch("http://127.0.0.1:8000/api/tournaments", {
-        method: 'POST',
+      const resp = await fetch(`${URL_ENVIROMENT}/api/tournaments/${tournamentId}`, {
+        method: 'PUT',
         headers: {
           Authorization: `Bearer ${token}`,
           'Content-Type': 'application/json',
           Accept: "application/json",
           withCredentials: "true",
         }, 
-        body: JSON.stringify(formDataValues)
+        body: JSON.stringify(cleanedValues)
       })
 
       const data = await resp.json()
@@ -131,18 +102,18 @@ const Tournaments = () => {
   
 
   return (
-    <section className="container mx-auto py-12 xl:px-0 sm:px-16 px-2 mb-16">
-        <div>
-            {!torneoInfo ? (
+    <section className="container min-h-[58vh] mx-auto py-12 xl:px-0 sm:px-16 px-2 mb-16">
+          <div className='min-h-[58vh]'>
+            {!tournamentInfo ? (
                 <h1 className="text-secondary text-4xl font-monse font-semibold">⌛ Cargando...</h1>
-            ) : torneoInfo.tournament ? (
+            ) : tournamentInfo ? (
                 <div className="flex flex-col gap-6">
                   <div className="flex items-center justify-center h-[150px] sm:h-[300px] w-full object-cover shadow-lg">
-                    <img src={torneoInfo.tournament.image} alt="Cover Torneo" className='object-cover h-[150px] sm:h-[300px] w-full rounded-xl'/>
+                    <img src={tournamentInfo.image} alt="Cover Torneo" className='object-cover h-[150px] sm:h-[300px] w-full rounded-xl'/>
                   </div>
                   <div className="name">
                     <h1 className="font-monse font-bold text-balance ss:text-[52px] text-[30px] text-secondary mb-1">
-                      {torneoInfo.tournament.name}
+                      {tournamentInfo.name}
                     </h1>
                   </div>
                   {!isAdmin && 
@@ -151,29 +122,29 @@ const Tournaments = () => {
                         <h1 className="font-monse font-bold text-balance ss:text-[30px] text-[22px] text-secondary mb-2">Fecha y hora de comienzo</h1>
                         <div className="flex flex-row items-center gap-2 mb-2">
                             <img src={calendar} alt="Calendar" width="26" height="auto"/>
-                            <span className="font-monse font-semibold text-[20px] sm:text-[26px] text-[#BFC0E0]">{formatDate(torneoInfo.tournament.starts_the).date}</span>
+                            <span className="font-monse font-semibold text-[20px] sm:text-[26px] text-[#BFC0E0]">{formatDate(tournamentInfo.starts_the).date}</span>
                         </div>
                         <div className="flex flex-row items-center gap-2 mb-4">
                             <img src={time} alt="Calendar" width="30" height="auto"/>
-                            <span className="font-monse font-semibold text-[20px] sm:text-[26px] text-[#BFC0E0]">{formatDate(torneoInfo.tournament.starts_the).time}</span>
+                            <span className="font-monse font-semibold text-[20px] sm:text-[26px] text-[#BFC0E0]">{formatDate(tournamentInfo.starts_the).time}</span>
                         </div>
                       </div>  
                       <div className="flex flex-col items-start sm:items-end">
                         <h1 className="font-monse font-bold text-balance ss:text-[30px] text-[22px] text-secondary mb-2">Participantes</h1>
-                        <span className="font-monse font-semibold text-[#BFC0E0] text-[20px] sm:text-[26px]">{torneoInfo.tournament.quantity_teams}</span>
+                        <span className="font-monse font-semibold text-[#BFC0E0] text-[20px] sm:text-[26px]">{tournamentInfo.quantity_teams}</span>
                       </div>  
                       <div className="flex flex-col items-start">
                         <h1 className="font-monse font-bold text-balance ss:text-[30px] text-[22px] text-secondary mb-2">Modalidad</h1>
-                        <span className="font-monse font-semibold text-[#BFC0E0] text-[20px] sm:text-[26px]">{torneoInfo.tournament.modality}</span>
+                        <span className="font-monse font-semibold text-[#BFC0E0] text-[20px] sm:text-[26px]">{tournamentInfo.modality}</span>
                       </div>  
                       <div className="flex flex-col items-start sm:items-end">
                         <h1 className="font-monse font-bold text-balance ss:text-[30px] text-[22px] text-secondary mb-2">Torneo realizado por</h1>
-                        <span className="font-monse font-semibold text-[#BFC0E0] text-[20px] sm:text-[26px]">{torneoInfo.tournament.managed_by.nick_name}</span>
+                        <span className="font-monse font-semibold text-[#BFC0E0] text-[20px] sm:text-[26px]">{tournamentInfo.managed_by.nick_name}</span>
                       </div>  
-                      {torneoInfo.tournament.link_ingame && (
+                      {tournamentInfo.link_ingame && (
                         <div className="flex flex-col items-start">
                           <h1 className="font-monse font-bold text-balance ss:text-[30px] text-[22px] text-secondary mb-2">Link del torneo</h1>
-                          <span className="font-monse font-semibold text-[#BFC0E0] text-[20px] sm:text-[26px]">{torneoInfo.tournament.link_ingame}</span>
+                          <span className="font-monse font-semibold text-[#BFC0E0] text-[20px] sm:text-[26px]">{tournamentInfo.link_ingame}</span>
                         </div>
                       )}
                       <div className="flex justify-center sm:justify-start ">
@@ -191,24 +162,19 @@ const Tournaments = () => {
                       </div> 
                     </div>
                   }{isAdmin && 
-                    <form id="createTournament"  onSubmit={handleSubmit(onSubmit)} >
+                    <form id="updateTournament"  onSubmit={handleSubmit(onSubmit)} >
                       <div className="grid grid-cols-1 sm:grid-cols-2 sm:grid-rows-3 gap-y-4">
                         <div className="flex flex-col items-start">
                           <h1 className="font-monse font-bold text-balance ss:text-[30px] text-[22px] text-secondary mb-2">Fecha y hora de comienzo</h1>
                         
                           <input type="datetime-local" name="starts_the" id="starts_the" className={`${inputElement} w-[211px] sm:w-auto`}
-                            {...register("starts_the", {
-                              required: {
-                                value: true,
-                                message: "La fecha de comienzo es requerida",
-                              }
-                            })
+                            {...register("starts_the")
                           }
                           />
                         </div>  
                         <div className="flex flex-col items-start sm:items-end">
                           <h1 className="font-monse font-bold text-balance ss:text-[30px] text-[22px] text-secondary mb-2">Participantes</h1>
-                          <input type="number" min={8} max={16} step={8} name="quantity_teams" id="quantity_teams" className={`${inputElement} w-[150px]`}
+                          <input type="number" placeholder={tournamentInfo.quantity_teams} min={8} max={16} step={8} name="quantity_teams" id="quantity_teams" className={`${inputElement} w-[150px]`}
                             {...register("quantity_teams")
                           }
                           />
@@ -216,15 +182,8 @@ const Tournaments = () => {
                         <div className="flex flex-col items-start">
                           <h1 className="font-monse font-bold text-balance ss:text-[30px] text-[22px] text-secondary mb-2">Modalidad</h1>
                           <select name="modality" className={`${inputElement} w-[211px] sm:w-auto`}
-                            {...register("modality", {
-                                required: {
-                                  value: true,
-                                  message: "La modalidad es requerida",
-                                } 
-                              })
-                            }
+                            {...register("modality")}
                           >
-                            <option value="">Seleccione una modalidad</option>
                             <option value="1v1">1v1</option>
                             <option value="2v2">2v2</option>
                             <option value="3v3">3v3</option>
@@ -234,11 +193,11 @@ const Tournaments = () => {
                         </div>
                         <div className="flex flex-col items-start sm:items-end">
                           <h1 className="font-monse font-bold text-balance ss:text-[30px] text-[22px] text-secondary mb-2">Torneo realizado por</h1>
-                          <span className="font-monse font-semibold text-[#BFC0E0] text-[20px] sm:text-[26px]">{torneoInfo.tournament.managed_by.nick_name}</span>
+                          <span className="font-monse font-semibold text-[#BFC0E0] text-[20px] sm:text-[26px]">{tournamentInfo.managed_by.nick_name}</span>
                         </div>  
                         <div className="flex flex-col basis-2/4">
                           <h1 className="font-monse font-bold text-balance ss:text-[30px] text-[22px] text-secondary mb-2">Link del torneo (opcional)</h1>
-                          <input type="text" name="link_ingame" id="link_ingame" className={`${inputElement}`} {...register("link_ingame")}/>
+                          <input type="text" placeholder={tournamentInfo.link_ingame} name="link_ingame" id="link_ingame" className={`${inputElement}`} {...register("link_ingame")}/>
                         </div>
 
                         <div className="flex flex-col items-center sm:items-end justify-end">
